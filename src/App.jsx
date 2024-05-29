3e3988bd4e405f70b758d2044ee99c67bb1b1800
Wesley <147221712+Wesleyweb30@users.noreply.github.com>
2024-05-29 17:54:03 -0300

    update portifolio my about with animation

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,9 @@ const App = () => (
       <Header />
     </header>
 
-    <main>
-      <Container className="mt-5 pt-5 ">
-        <About />
-
-        {/* <section>
-          <h2 className="my-5">Projetos</h2>
-          <ProjectCarousel />
-          <Row>
-            <Col md={4}>
-              <ProjectCard
-                title="Projeto 1"
-                description="Descrição do Projeto 1"
-                imgSrc="projeto1.jpg"
-                link="https://github.com/seu-usuario/projeto1"
-              />
-            </Col>
-            <Col md={4}>
-              <ProjectCard
-                title="Projeto 2"
-                description="Descrição do Projeto 2"
-                imgSrc="projeto2.jpg"
-                link="https://github.com/seu-usuario/projeto2"
-              />
-            </Col>
-            <Col md={4}>
-              <ProjectCard
-                title="Projeto 3"
-                description="Descrição do Projeto 3"
-                imgSrc="projeto2.jpg"
-                link="https://github.com/seu-usuario/projeto2"
-              />
-            </Col>
-          </Row>
-
-        </section>
-
-        <section>
-          <ContactForm />
-        </section> */}
-
+    <main  className="d-flex align-items-center justify-content-center vh-100">
+      <Container className="pt-5 ">
+          <About />
       </Container>
     </main>
 
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,31 +1,52 @@
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faJava, faBootstrap } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { useSpring, animated } from '@react-spring/web';
+import './about.css';
 
-const About = () => (
-  <>
-    <Container className="my-3">
+export default function About() {
+  const springs = useSpring({
+    from: { opacity: 0, transform: 'translateY(20px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    delay: 300,
+  });
+
+  const iconSprings = useSpring({
+    from: { opacity: 0, transform: 'scale(0.5)' },
+    to: { opacity: 1, transform: 'scale(1)' },
+    delay: 500,
+  });
+
+  return (
+    <>
+    <Container className="my-3 p-4 bg-body-tertiary" >
       <Row>
         <Col md={4}>
           <Image src="src/assets/perfil-portifolio.jpg" roundedCircle fluid />
         </Col>
+
         <Col md={8}>
-          <h2>About</h2>
-          <p>
-            {"Hi, my name is Wesley Luan, I 'm a full-stack developer I love creating things to solve real-life problems for digital. I currently develop with springboot, react js and bootstrap"}
-          </p>
-          <p>
-            My Skins Developer
-            <div className="mt-3 d-flex gap-3">
-              <FontAwesomeIcon icon={faReact} size="2x" className="mr-2" />
-              <FontAwesomeIcon icon={faJava} size="2x" className="mr-2" />
-              <FontAwesomeIcon icon={faBootstrap} size="2x" className="mr-2" />
-            </div>
-          </p>
+          <section>
+            <Container fluid>
+              <animated.h2 style={springs} className="about-title">About</animated.h2>
+              <animated.p style={springs} className="about-text">
+                Hi, my name is Wesley Luan. I am a full-stack developer. I love creating things to solve real-life problems for digital. I currently develop with Spring Boot, React.js, and Bootstrap.
+              </animated.p>
+
+              <animated.div style={iconSprings} className="mt-3 d-flex align-items-center gap-3">
+                <h4 className="skills-title">My Social Media</h4>
+                <a href="https://github.com/Wesleyweb30" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faGithub} size="2x" className="icon" />
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faLinkedin} size="2x" className="icon" />
+                </a>
+              </animated.div>
+            </Container>
+          </section>
         </Col>
       </Row>
     </Container>
-  </>
-);
-
-export default About;
+    </>
+  );
+}
